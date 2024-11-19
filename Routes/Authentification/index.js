b2d4671364c18664c2c003e2a8b5cfe150f4bf95
Elie Ruvinga <ruvingaelie@gmail.com>
2024-11-19 16:00:55 +0200

    updating Routes for Administration datas

--- Routes/Authentification/index.js
+++ Routes/Authentification/index.js
@@ -2,8 +2,9 @@
 const router = require("express").Router();
 const CtrlUser = require("../../Controllers/Authentification")
 
+router.post("/AdminLogin", CtrlUser.Adminlogin);
 router.post("/Login", CtrlUser.login);
 router.post("/ActiveAccount", CtrlUser.Activation_account);
-router.post("/newUser", CtrlUser.registerNewUser);
+router.post("/CreateUser", CtrlUser.registerNewUser);
 
 module.exports = router;
